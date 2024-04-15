836c47cbbf4b455430d5ce10830886731e6f5b7d
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2024-04-15 09:29:16 +0200

    added jest to run test

--- src/components/common/AlsoPurchased.vue
+++ src/components/common/AlsoPurchased.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="container lg:mx-20 xl:mx-auto relative">
+  <div class="container pt-20 lg:mx-20 xl:mx-auto relative">
     <div
       class="md:w-2/3 w-full pb-6 md:pb-0 md:pr-6 flex-col md:block flex items-center justify-center md:pt-0 pt-4"
     >
-      <h1
-        class="mt-5 lg:mt-12 py-3 lg:py-4 px-4 lg:px-6 font-bold text-black rounded-lg text-sm lg:text-lg xl:text-xl hover:opacity-90"
-      >
-        Recommended
-      </h1>
+      <router-link to="/AllTshirt">
+        <button
+          @click="changeButtonText"
+          :style="{ backgroundImage: buttonBackgroundGradient }"
+          class="moving-button w-full shadow-xl hover:scale-105 lg:py-4 rounded-full px-4 lg:px-4 font-bold text-gray-600 text-md lg:text-md xl:text-md hover:opacity-90"
+        >
+          {{ buttonText }}
+        </button>
+      </router-link>
     </div>
 
     <div class="flex items-center justify-center w-full h-full py-24 sm:py-8 px-4">
@@ -16,7 +20,7 @@
           <div ref="slider" class="keen-slider">
             <div class="keen-slider__slide number-slide1">
               <img
-                class="object-contain h-full w-full bg-gradient-to-r from-green-300 to-purple-400"
+                class="object-contain h-full w-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"
                 :src="require('@/assets/HotNow.png')"
               />
             </div>
@@ -28,25 +32,25 @@
             </div>
             <div class="keen-slider__slide number-slide3">
               <img
-                class="object-contain h-full w-full bg-gradient-to-r from-green-300 to-purple-400"
+                class="object-contain h-full w-full bg-gradient-to-r from-yellow-200 via-pink-200 to-pink-400"
                 :src="require('@/assets/HotNow.png')"
               />
             </div>
             <div class="keen-slider__slide number-slide4">
               <img
-                class="object-contain h-full w-full bg-gradient-to-r from-green-300 to-purple-400"
+                class="object-contain h-full w-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500"
                 :src="require('@/assets/HotNow4.png')"
               />
             </div>
             <div class="keen-slider__slide number-slide5">
               <img
-                class="object-contain h-full w-full bg-gradient-to-r from-green-300 to-purple-400"
+                class="object-contain h-full w-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"
                 :src="require('@/assets/HotNow2.png')"
               />
             </div>
             <div class="keen-slider__slide number-slide6">
               <img
-                class="object-contain h-full w-full bg-gradient-to-r from-green-300 to-purple-400"
+                class="object-contain h-full w-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500"
                 :src="require('@/assets/HotNow4.png')"
               />
             </div>
@@ -65,6 +69,8 @@ export default {
   components: {},
   name: "TheSlider",
   mounted() {
+    setInterval(this.changeButtonText, 5000);
+    setInterval(this.changeButtonGradient, 5000);
     this.slider = new KeenSlider(
       this.$refs.slider,
       {
@@ -104,6 +110,43 @@ export default {
   },
   beforeDestroy() {
     if (this.slider) this.slider.destroy();
+  },
+  methods: {
+    navigateToNewItems() {
+      // Navigate to the route for new items
+      this.$router.push({ name: "NewItems" });
+    },
+    changeButtonText() {
+      const randomIndex = Math.floor(Math.random() * this.randomTexts.length);
+      this.buttonText = this.randomTexts[randomIndex];
+    },
+    changeButtonGradient() {
+      const randomIndex = Math.floor(Math.random() * this.randomGradients.length);
+      this.buttonBackgroundGradient = this.randomGradients[randomIndex];
+    },
+  },
+  data() {
+    return {
+      buttonText: "New steeze just arrived! Check them out",
+      randomTexts: [
+        "Fresh items just landed!",
+        "Check out the latest arrivals!",
+        "New styles available now!",
+        "Discover trendy new arrivals!",
+        "Exciting new collection now in stock!",
+        "Explore our new arrivals!",
+      ],
+      randomGradients: [
+        "linear-gradient(to right, rgb(254, 240, 138), rgb(251, 207, 232), rgb(244, 114, 182))",
+        "linear-gradient(to right, rgb(134, 239, 172), rgb(192, 132, 252))",
+        "linear-gradient(to right, rgb(254, 240, 138), rgb(251, 207, 232), rgb(244, 114, 182))",
+        "linear-gradient(to right, rgb(134, 239, 172), rgb(192, 132, 252))",
+        "linear-gradient(to right, rgb(254, 240, 138), rgb(251, 207, 232), rgb(244, 114, 182))",
+        "linear-gradient(to right, rgb(134, 239, 172), rgb(192, 132, 252))",
+      ],
+      buttonBackgroundGradient:
+        "linear-gradient(to right, rgb(254, 240, 138), rgb(251, 207, 232), rgb(244, 114, 182))", // Initial background color
+    };
   },
 };
 </script>
@@ -174,6 +217,26 @@ export default {
 .carousel__sliderLarge {
   padding-left: 20%;
   padding-right: 20%;
+}
+
+.moving-button {
+  position: relative;
+  animation: moveRight 15s infinite;
+  transform: translateZ(0);
+}
+
+@keyframes moveRight {
+  0%,
+  100% {
+    left: 0;
+  }
+  50% {
+    left: calc(50%); /* Adjust the width of the button */
+  }
+}
+
+.moving-button:hover {
+  animation-play-state: paused; /* Pause animation on hover */
 }
 </style>
 
